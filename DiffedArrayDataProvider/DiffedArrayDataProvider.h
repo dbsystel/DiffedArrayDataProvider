fef8ee676c5d37b73a1e430d379be34bf8d3aeca
Lukas Schmidt <lukas.la.schmidt@deutschebahn.com>
2017-09-27 06:24:07 +0200

    Initial Commit

---
+++ DiffedArrayDataProvider/DiffedArrayDataProvider.h
@@ -0,0 +1,19 @@
+//
+//  DiffedArrayDataProvider.h
+//  DiffedArrayDataProvider
+//
+//  Created by Lukas Schmidt on 27.09.17.
+//  Copyright © 2017 DBSystel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DiffedArrayDataProvider.
+FOUNDATION_EXPORT double DiffedArrayDataProviderVersionNumber;
+
+//! Project version string for DiffedArrayDataProvider.
+FOUNDATION_EXPORT const unsigned char DiffedArrayDataProviderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DiffedArrayDataProvider/PublicHeader.h>
+
+
